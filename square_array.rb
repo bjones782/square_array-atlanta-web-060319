1faeb7654fa3ec81835bee1391fce6086efc7a68
Brooke Jones <bjones782@gmail.com>
2019-05-13 18:48:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  numbers = [1, 2, 3]
+  array.each do |element|
+    
+end
+
+
+def square_array(array)
+    new_array = []
+    array.each do |element|
+        new_array << element ** 2
+    end
+    new_array
 end
